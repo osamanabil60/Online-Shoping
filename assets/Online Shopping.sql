139e64fd4eb6e752ac66d9a43f5b53c9746cf9b8
Yossef Ibrahim Mohamed-Salah <129223522+yossefibrahimmohamed@users.noreply.github.com>
2025-03-11 03:21:20 +0200

    Uploaded
    This is a Online Shopping project using
    HTML
    CSS
    Java Script
    and PHP

---
+++ assets/Online Shopping.sql
@@ -0,0 +1,16 @@
+create database Online_Shopping;
+use Online_Shopping
+create table Registration
+(
+	User_Name_ varchar(max) ,
+	password_ int primary key Not Null,
+	
+	
+)
+	insert into Registration values(	
+	'Yossef@scal.local',220190206	,
+	'Joseph@scal.local', 220190409 ,
+	'Abdelrahman@scal.local',220190217,
+	'Osama Nabi@scal.local',220190475,
+	'Ahmed@scal.local',220190223 ,
+	'Mostafa@scal.local',220190157)
